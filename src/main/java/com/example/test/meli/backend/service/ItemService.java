d3e58046fedd83c4474fd4c63553f514af2dedeb
Samuel Chagas <60257787+SamuelCezar@users.noreply.github.com>
2025-07-03 11:56:12 -0300

    Adicionando log.info

--- src/main/java/com/example/test/meli/backend/service/ItemService.java
+++ src/main/java/com/example/test/meli/backend/service/ItemService.java
@@ -4,15 +4,19 @@ import com.example.test.meli.backend.exception.ItemNotFoundException;
 import com.example.test.meli.backend.model.Item;
 import com.example.test.meli.backend.repository.ItemRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ItemService {
 
     private final ItemRepository itemRepository;
 
     public Item getItemById(String id) {
+        log.info("Getting item by id: " + id);
+
         return itemRepository.findAll().stream()
                 .filter(item -> item.getId().equalsIgnoreCase(id))
                 .findFirst()
